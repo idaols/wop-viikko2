35eae1a899264b5a5a88d4312242356073ddf16d
idaols <ida.olsson@metropolia.fi>
2021-11-01 15:44:48 +0200

    first commit

---
+++ controllers/catController.js
@@ -0,0 +1,27 @@
+"use strict";
+// catController
+
+const catModel = require("../models/catModel");
+
+//const cats = catModel.cats; , sama lyhyemmin alla
+const { cats, getCat } = catModel;
+
+const cat_list_get = (req, res) => {
+  res.json(cats);
+};
+
+const cat_get = (req, res) => {
+  const vastaus = getCat(req.params.id);
+  res.json(vastaus);
+};
+
+const cat_post = (req, res) => {
+  console.log(req.body, req.file);
+  res.send("With this endpoint you can add cats");
+};
+
+module.exports = {
+  cat_list_get,
+  cat_get,
+  cat_post,
+};
